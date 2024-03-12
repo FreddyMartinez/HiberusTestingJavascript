90d3ab39dff91819236649a4cd7f5ee6c4b5581c
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-12 18:44:28 +0100

    test: reducer function

---
+++ __tests__/reduce.test.js
@@ -0,0 +1,20 @@
+// Function should receive an array and a callback
+// Function should apply the callback to each element in the array
+
+describe("Reduce", () => {
+  it("should receive array of number and return sum of numbers", () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+
+    const callbackSum = (acc, current) => {
+      return acc + current;
+    };
+
+    const callbackMult = (acc, current) => {
+      return acc * current;
+    };
+
+    expect(typeof reduceNumbers(numbers, callbackSum)).toBe("number");
+    expect(reduceNumbers(numbers, callbackSum)).toBe(21);
+    expect(reduceNumbers(numbers, callbackMult)).toBe(720);
+  });
+});
