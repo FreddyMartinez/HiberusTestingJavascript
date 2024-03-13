119127390920c9ee00311ff8e09740746afbd1ce
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-13 19:22:36 +0100

    test: validate user data

--- simple-node-serve/__tests__/signUp.test.ts
+++ simple-node-serve/__tests__/signUp.test.ts
@@ -25,13 +25,30 @@ describe("Signup endpoint", () => {
   });
 
   it.each([
-    ["username"],
-    ["email"],
-    ["password"],
-  ])("should return 400 when no %s is sent", async (param, ) => {
-    const incompleteUser: Record<string, string> = {...user};
-    delete incompleteUser[param];
-    const res = await request(app).post(USER_ENDPOINT).send(incompleteUser);
-    expect(res.status).toBe(400);
+    ["username", "username is required"],
+    ["email", "email is required"],
+    ["password", "password is required"],
+  ])(
+    "should return 400 with message when no %s is sent",
+    async (param, message) => {
+      const incompleteUser: Record<string, string> = { ...user };
+      delete incompleteUser[param];
+      const res = await request(app).post(USER_ENDPOINT).send(incompleteUser);
+      expect(res.status).toBe(400);
+      const validationErrors: Record<string, string> = {};
+      validationErrors[param] = message;
+      expect(res.body).toEqual({ validationErrors });
+    }
+  );
+
+  it("Should return validation errors when several params are missing", async () => {
+    const emptyUser = {};
+    const res = await request(app).post(USER_ENDPOINT).send(emptyUser);
+    const validationErrors: Record<string, string> = {
+      username: "username is required",
+      email: "email is required",
+      password: "password is required",
+    };
+    expect(res.body).toEqual({ validationErrors });
   });
 });
--- simple-node-serve/src/index.ts
+++ simple-node-serve/src/index.ts
@@ -7,8 +7,19 @@ app.use(express.json());
 app.post(USER_ENDPOINT, (req, res) => {
   const { username, email, password } = req.body;
 
-  if(!username || !email || !password) {
-    return res.status(400).send('')
+
+  const validationErrors: Record<string, string> = {};
+  if (!username) {
+    validationErrors["username"] = "username is required";
+  } 
+  if (!email) {
+    validationErrors["email"] = "email is required";
+  } 
+  if (!password) {
+    validationErrors["password"] = "password is required";
+  }
+  if (Object.keys(validationErrors).length) {
+    return res.status(400).send({validationErrors});
   }
 
   res.send({ message: "User registered"});
