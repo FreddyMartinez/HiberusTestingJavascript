2c6aec82114ef00b9e794e0ac73481092dbc7db2
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-20 19:56:59 +0100

    refactor: remove unused code

--- simple-node-serve/src/bll/user.ts
+++ simple-node-serve/src/bll/user.ts
@@ -1,29 +1,17 @@
 import { User } from "../dal/user";
 import { encrypt, generateSalt } from "../../util/encrypt";
-import { USER_MESSAGES } from "../../util/constants";
 
 async function saveUser(user: UserData) {
   // generate salt
   const salt = await generateSalt();
   // encrypt password
   const pass = await encrypt(user.password, salt);
-  try {
-    await User.create({...user, salt, password: pass });
-  } catch (error) {
-    throw new BdConnectionError("Error saving user in database");
-  }
+  await User.create({...user, salt, password: pass });
 }
 
 async function emailExistsInBd(email: string) {
   const user = await User.findOne({ where: { email } });
   return !!user;
-}
-
-export class BdConnectionError extends Error {
-  name = "BdConnectionError";
-  constructor(message: string) {
-    super(message);
-  }
 }
 
 export { saveUser, emailExistsInBd };
